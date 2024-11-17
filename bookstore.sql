ea20849a4e8adfd36c5e97d7670ac6278218b8f8
amircoderf <155719980+amircoderf@users.noreply.github.com>
2024-11-17 23:17:42 +0800

    Update bookstore.sql

--- bookstore.sql
+++ bookstore.sql
@@ -1,31 +1,4 @@
--- phpMyAdmin SQL Dump
--- version 5.2.1
--- https://www.phpmyadmin.net/
---
--- Host: 127.0.0.1
--- Generation Time: Nov 17, 2024 at 04:13 PM
--- Server version: 10.4.32-MariaDB
--- PHP Version: 8.2.12
-
-SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
-START TRANSACTION;
-SET time_zone = "+00:00";
-
-
-/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
-/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
-/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
-/*!40101 SET NAMES utf8mb4 */;
-
---
--- Database: `bookstore`
---
-
--- --------------------------------------------------------
-
---
 -- Table structure for table `book`
---
 
 CREATE TABLE `book` (
   `ISBN` varchar(15) NOT NULL,
@@ -39,9 +12,7 @@ CREATE TABLE `book` (
 
 -- --------------------------------------------------------
 
---
--- Table structure for table `user`
---
+Table structure for table `user`
 
 CREATE TABLE `user` (
   `UserID` int(11) NOT NULL,
@@ -91,6 +62,3 @@ ALTER TABLE `user`
   MODIFY `UserID` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
 COMMIT;
 
-/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
-/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
-/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
